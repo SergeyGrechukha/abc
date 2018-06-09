f7255294ebf50a1cad5b55c2a7c0d1b9e681a3eb
Sergiy Grechukha <sergey.grechukha@gmail.com>
2018-06-09 19:00:12 +0300

    Added tts

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:abc/home_page.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:tts/tts.dart';
+import 'package:flutter/material.dart';
 
 
 void main() async {
